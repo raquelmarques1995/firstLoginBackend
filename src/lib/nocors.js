834a5de499db57af624c7b93787a28c00d0d2e10
raquelmarques1995 <163328182+raquelmarques1995@users.noreply.github.com>
2025-02-25 15:17:35 +0000

    first commit

---
+++ src/lib/nocors.js
@@ -0,0 +1,16 @@
+//
+
+export function allowCors(handler){
+    return async (req, res) => {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT, DELETE");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+        if (req.method == "OPTIONS"){
+            return res.status(200).end(); //Retorna 200 para todas as requisições
+        }
+
+        return handler(req, res);
+    };
+}
